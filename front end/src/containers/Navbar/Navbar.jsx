eb00f5515eec6082868ed4404dc777480a339c87
MaaX59 <maax.strandberg@gmail.com>
2024-04-17 12:43:45 +0200

    navbar

--- front end/src/containers/Navbar/Navbar.jsx
+++ front end/src/containers/Navbar/Navbar.jsx
@@ -9,6 +9,22 @@ const Navbar = () => {
         <img src={logo} alt="logo" />
         <h4>StrandBlog</h4>
       </div>
+      <div className="navbar_links">
+        <ul>
+          <li>
+            <a href="#home">Home</a>{" "}
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#gallery">Gallery</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
