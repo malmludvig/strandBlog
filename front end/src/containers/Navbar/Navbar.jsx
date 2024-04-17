82c6f8a446507f24c62f27e5eb7dd511431f9194
MaaX59 <maax.strandberg@gmail.com>
2024-04-17 12:22:06 +0200

    logo added for navbar

--- front end/src/containers/Navbar/Navbar.jsx
+++ front end/src/containers/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import { React } from "react";
 import "./Navbar.css";
+import logo from "../../images/sand_PNG.png";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <div className="navbar_logo">
+        <img src={logo} alt="logo" />
+        <h4>StrandBlog</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
