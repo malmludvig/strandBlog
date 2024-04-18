3724753c6587384a21eb2ce92785b399a3d25ae7
MaaX59 <maax.strandberg@gmail.com>
2024-04-18 20:23:52 +0200

    more login, adding react icons

--- front end/src/Pages/Login/Login.jsx
+++ front end/src/Pages/Login/Login.jsx
@@ -3,6 +3,9 @@ import { React, useState } from "react";
 import axios from "axios";
 import { server } from "../../server";
 
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
+
 import { Navbar } from "../../containers";
 import "./login.css";
 
@@ -11,12 +14,14 @@ const Login = () => {
   const [emailLogin, setEmailLogin] = useState("");
   const [passwordLogin, setPasswordLogin] = useState("");
   const [errorLogin, setErrorLogin] = useState("");
+  const [showPasswordLogin, setShowPasswordLogin] = useState(false);
 
   //sign Up states
   const [emailSignup, setEmailSignup] = useState("");
   const [passwordSignup, setPasswordSignup] = useState("");
   const [nameSignup, setNameSignup] = useState("");
   const [errorSignUp, setErrorSignUp] = useState("");
+  const [showPasswordSignup, setShowPasswordSignup] = useState(false);
 
   //Login
   const handleLogin = async (e) => {
@@ -36,6 +41,7 @@ const Login = () => {
         console.log(response.data);
         setErrorLogin("This email dosen´t exist");
       } else {
+        //expects a authentication token from backend
         console.log("token -->", response.data.token);
         const token = response.data.token;
         const userInfoFromDb = response.data.userInfo;
@@ -82,62 +88,65 @@ const Login = () => {
       <div className="login_content">
         <div className="login_content_login">
           <h2>Welcome back</h2>
-          <span>Log in to access your account.</span>
+
           <form onSubmit={handleLogin}>
-            <lable for="email">Your Email</lable>
+            <lable for="email">Your Email*</lable>
             <input
               type="email"
-              placeholder="  Enter Email"
               name="email"
+              required
               onChange={(event) => {
                 setEmailLogin(event.target.value);
               }}
             ></input>
-            <lable for="pwd"> Your Password</lable>
+            <lable for="pwd"> Your Password*</lable>
             <input
-              type="password"
-              placeholder="  Enter Password"
+              type={showPasswordLogin ? "text" : "password"}
               name="pwd"
+              required
               onChange={(event) => {
                 setPasswordLogin(event.target.value);
               }}
             ></input>
+            <div className="form-required">* field is required</div>
             <span>{errorLogin && errorLogin}</span>
             <button type="submit">Login</button>
           </form>
         </div>
+
         <div className="login_content_signup">
           <h2>Join the community</h2>
           <form onSubmit={handleSignup}>
-            <lable for="name">Full Name</lable>
+            <lable for="name">Full Name*</lable>
             <input
               type="text"
-              placeholder="  Enter your full name"
               name="name"
+              required
               onChange={(event) => {
                 setNameSignup(event.target.value);
               }}
             ></input>
 
-            <lable for="email">Email address</lable>
+            <lable for="email">Email address*</lable>
             <input
               type="email"
-              placeholder="  Enter your email"
               name="email"
+              required
               onChange={(event) => {
                 setEmailSignup(event.target.value);
               }}
             ></input>
 
-            <lable for="pwd">Password</lable>
+            <lable for="pwd">Password*</lable>
             <input
-              type="password"
-              placeholder="  Enter your password"
+              type={showPasswordSignup ? "text" : "password"}
               name="pwd"
+              required
               onChange={(event) => {
                 setPasswordSignup(event.target.value);
               }}
             ></input>
+            <div className="form-required">* field is required</div>
             <span>{errorSignUp && errorSignUp}</span>
             <button type="submit">Sign up</button>
           </form>
