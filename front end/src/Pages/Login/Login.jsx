6251a9cb5e2277202d982aa2c011d3d898b425be
MaaX59 <maax.strandberg@gmail.com>
2024-04-18 12:16:34 +0200

    worked on login

--- front end/src/Pages/Login/Login.jsx
+++ front end/src/Pages/Login/Login.jsx
@@ -6,7 +6,40 @@ const Login = () => {
   return (
     <div className="login">
       <Navbar />
-      <div className="login_content"></div>
+      <div className="login_content">
+        <div className="login_content_login">
+          <h2>Welcome back</h2>
+          <span>Log in to access your account.</span>
+          <form>
+            <lable for="email">Your Email</lable>
+            <input
+              type="email"
+              placeholder="  Enter Email"
+              name="email"
+            ></input>
+            <lable for="pwd"> Your Password</lable>
+            <input
+              type="password"
+              placeholder="  Enter Password"
+              name="pwd"
+            ></input>
+            <button type="button">Login</button>
+          </form>
+        </div>
+        <div className="login_content_signup">
+          <h2>Join our community</h2>
+          <form>
+            <lable></lable>
+            <input></input>
+            <lable></lable>
+            <input></input>
+            <lable></lable>
+            <input></input>
+            <lable></lable>
+            <input></input>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
